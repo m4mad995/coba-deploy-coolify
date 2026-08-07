a07418203be5cb27951ea6c2af9d85d8df8b278e
m4mad995 <209729772+m4mad995@users.noreply.github.com>
2026-08-07 13:35:01 +0700

    kembalikan spt normal

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => redirect()->route('todos.index'));
 
 Route::resource('todos', TodoController::class)->only(['index', 'store', 'update', 'destroy']);
-
-Route::get('/debug-glitchtip', function () {
-    throw new Exception('Test GlitchTip error!');
-});
